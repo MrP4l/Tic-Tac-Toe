3e86f24f3b22817061ca2edc5ec8aa746cef5206
MrP4l <101936971+MrP4l@users.noreply.github.com>
2023-03-25 00:05:39 +0100

    FNC - Friday Night Coding (Ouch)

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-let Player1 = document.getElementById("playerOneName");
-let Player2 = document.getElementById("playerTwoName");
+let playerOne = document.getElementById("playerOneName");
+let playerTwo = document.getElementById("playerTwoName");
 let playButton = document.getElementById("playButton");
+let playerOneName = document.getElementById("nameOne");
+let playerTwoName = document.getElementById("nameTwo");
 
 const Player = name => {
     const sayName = () => console.log(name);
@@ -9,15 +11,62 @@ const Player = name => {
 
 const Gameboard = (() => {
     let board = [];
+    let n = 0
     const squares = document.querySelectorAll(".square");
     squares.forEach(square => {
+        n += 1
+        square.dataset.index = n;
         square.addEventListener("click", () => {
-            console.log('hello')
+            //console.log(square.dataset.index)
         })
     });
-
-
 })();
+
+const Play = () => {
+    let playerOneChoices = [];
+    let playerTwoChoices = [];
+
+    const getNames = () => {
+            if (playerOne != null && playerTwo != null) {
+                let namePlayerOne = playerOne.value;
+                let namePlayerTwo = playerTwo.value;
+                console.log(namePlayerOne, namePlayerTwo)
+                
+                playerOneName.innerHTML = namePlayerOne + ":";
+                playerTwoName.innerHTML = namePlayerTwo + ":";
+            }
+        }
+    
+    const startTheGame = () => {
+        playButton.addEventListener("click", () => {
+            const gameOne = Play();
+            gameOne.getNames();
+        })
+    }
+
+    const click = () => {
+        const squares = document.querySelectorAll(".square");
+        squares.forEach(square => {
+            square.addEventListener("click", () => {
+                if (playerTwoChoices.length < playerOneChoices.length) {
+                    playerTwoChoices.push(square.dataset.index);
+                    console.log(playerOneChoices, playerTwoChoices);
+                }
+                playerTwoChoices.push(square.dataset.index);
+                console.log("P1:",playerOneChoices, "P2:",playerTwoChoices);
+            })
+        });
+    }
+        return {getNames, startTheGame, click};
+}
+
+const go = Play();
+go.startTheGame();
+go.click();
+
+
+
+
 
 const gameController = (() => {
 
